090741cf768e62593ac7e321155dd0c6461db0af
TumbaevAA <theonetommygun1@gmail.com>
2022-01-11 22:33:04 +0700

    two-dimensional array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Point[] pointArray = new Point[3];
+        Point[][] twoDimensionalArray = new Point[2][3];
+        for (int i = 0; i < twoDimensionalArray.length; i++)
+            for (int j = 0; j < twoDimensionalArray[i].length; j++) {
+                System.out.println("\nEnter coordinates of the " + j + " point in the " + i + " row\n");
+                twoDimensionalArray[i][j] = new Point();
+                twoDimensionalArray[i][j].input();
+                twoDimensionalArray[i][j].output();
+            }
 
+
+        Point[] pointArray = new Point[3];
         for (int i = 0; i < pointArray.length; i++) {
-            System.out.println("Enter coordinates of the " + i + " point\n");
+            System.out.println("\nEnter coordinates of the " + i + " point\n");
             pointArray[i] = new Point();
             pointArray[i].input();
             pointArray[i].output();
